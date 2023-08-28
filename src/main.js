24f23c5b3009d5c7ef1c499a38a81cb8db4507e6
ZubsZero <zubairmatthee262@gmail.com>
2023-08-28 14:40:18 +0200

    Update

--- src/main.js
+++ src/main.js
@@ -2,5 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import bootstrap from 'bootstrap'
 
-createApp(App).use(store).use(router).mount('#app')
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+
+createApp(App).use(store).use(router).use(bootstrap).mount('#app')
