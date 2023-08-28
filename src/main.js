e0cbc45a0784ac1d9facffb6b25095c81fc74f02
ZubsZero <zubairmatthee262@gmail.com>
2023-08-28 16:19:17 +0200

    Update

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import bootstrap from 'bootstrap'
+import 'bootstrap-icons/font/bootstrap-icons';
+
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
